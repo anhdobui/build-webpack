727009d435d7f82d2726005f5ecfe5a05487ac46
anhdobui <anhdobui2002@gmail.com>
2023-01-29 09:22:03 +0700

    set source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,46 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-module.exports = {
-  mode: 'development',
-  entry: {
-    app: path.resolve('src/index.js')
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss|css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+module.exports = (env) => {
+    const isDevelopment = Boolean(env.development)
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: {
+          app: path.resolve('src/index.js')
+        },
+        output: {
+          path: path.resolve(__dirname, 'dist'),
+          filename: '[name].[contenthash].js',
+          clean: true
+        },
+        module: {
+          rules: [
+            {
+              test: /\.s[ac]ss|css$/,
+              use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+          ]
+        },
+        devtool: isDevelopment ? 'source-map' : false,
+        plugins: [
+          new MiniCssExtractPlugin({
+              filename: '[name].[contenthash].css'
+          }),
+          new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+          })
+        ],
+        devServer: {
+          static: {
+            directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
+          },
+          port: 3000, // Port thay cho port mặc định (8080)
+          open: true, // Mở trang webpack khi chạy terminal
+          hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+          compress: true, // Bật Gzip cho các tài nguyên
+          historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+        }
       }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: 'src/template.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
-    },
-    port: 3000, // Port thay cho port mặc định (8080)
-    open: true, // Mở trang webpack khi chạy terminal
-    hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-    compress: true, // Bật Gzip cho các tài nguyên
-    historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
-  }
 }
