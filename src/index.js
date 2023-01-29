9cea65c45143b6447063c90127f555d19d796145
anhdobui <anhdobui2002@gmail.com>
2023-01-29 13:12:28 +0700

    up file .prittier and .vscode

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import 'core-js/modules/es.object.values'
-import 'core-js/modules/es.promise'
-import './styles/style.css'
-import domHandler from './dom'
-const sum = (num1,num2) => num1 + num2
-console.log(sum(100,10))
-domHandler()
+import 'core-js/modules/es.object.values';
+import 'core-js/modules/es.promise';
+import './styles/style.css';
+import domHandler from './dom';
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(100, 10));
+domHandler();
 // ES6 Spread Operator
-const person = { name: 'Do' }
-const personClone = { ...person }
-console.log('personClone', personClone)
+const person = { name: 'Do' };
+const personClone = { ...person };
+console.log('personClone', personClone);
 
 // ES7 Object.values
-console.log('Object.values', Object.values(personClone))
+console.log('Object.values', Object.values(personClone));
 
-const handle = (time) => new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(400)
-    },time)
-})
+const handle = (time) =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(400);
+        }, time);
+    });
 const main = async () => {
-    try{
-        const value = await handle()
-        console.log(value)
-    }catch{
-        console.log('co loi')
+    try {
+        const value = await handle();
+        console.log(value);
+    } catch {
+        console.log('co loi');
     }
-}
-main()
+};
+main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,81 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = (env) => {
-    const isDevelopment = Boolean(env.development)
+    const isDevelopment = Boolean(env.development);
     const basePlugins = [
         new MiniCssExtractPlugin({
-          filename: '[name].[contenthash].css'
+            filename: '[name].[contenthash].css',
         }),
         new HtmlWebpackPlugin({
-          title: 'Webpack App',
-          filename: 'index.html',
-          template: 'src/template.html'
-        })
-      ]
-      const plugins = isDevelopment
-    ? basePlugins
-    : [...basePlugins, new BundleAnalyzerPlugin()]
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ];
+    const plugins = isDevelopment ? basePlugins : [...basePlugins, new BundleAnalyzerPlugin()];
     return {
         mode: isDevelopment ? 'development' : 'production',
         entry: {
-          app: path.resolve('src/index.js')
+            app: path.resolve('src/index.js'),
         },
         output: {
-          path: path.resolve(__dirname, 'dist'),
-          filename: '[name].[contenthash].js',
-          clean: true
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            clean: true,
         },
         module: {
-          rules: [
-            {
-              test: /\.s[ac]ss|css$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.m?js$/,
-                exclude: {
-                  and: [/node_modules/], // Exclude libraries in node_modules ...
-                  not: [
-                    // Except for a few of them that needs to be transpiled because they use modern syntax
-                    /unfetch/,
-                    /d3-array|d3-scale/,
-                    /@hapi[\\/]joi-date/,
-                  ]
+            rules: [
+                {
+                    test: /\.s[ac]ss|css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
                 },
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                        [
-                            '@babel/preset-env',
-                            {
-                              debug: true, // Hiển thị debug lên terminal để dễ debug
-                              useBuiltIns: 'entry', // Dùng 'usage' thì đơn giản nhất, không cần import core-js vào code
-                                                    // Dùng 'entry' thì tự tay import giúp giảm dung lượng của file
-                              corejs: '3.27.2' // nên quy định verson core-js để babel-preset-env nó hoạt động tối ưu
-                            }
-                          ]
-                    ]
-                  }
-                }
-              },
-              {
-                test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
-                type: 'asset/resource'
-              }
-          ]
+                {
+                    test: /\.m?js$/,
+                    exclude: {
+                        and: [/node_modules/], // Exclude libraries in node_modules ...
+                        not: [
+                            // Except for a few of them that needs to be transpiled because they use modern syntax
+                            /unfetch/,
+                            /d3-array|d3-scale/,
+                            /@hapi[\\/]joi-date/,
+                        ],
+                    },
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        debug: true, // Hiển thị debug lên terminal để dễ debug
+                                        useBuiltIns: 'entry', // Dùng 'usage' thì đơn giản nhất, không cần import core-js vào code
+                                        // Dùng 'entry' thì tự tay import giúp giảm dung lượng của file
+                                        corejs: '3.27.2', // nên quy định verson core-js để babel-preset-env nó hoạt động tối ưu
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+                    type: 'asset/resource',
+                },
+            ],
         },
         devtool: isDevelopment ? 'source-map' : false,
         plugins,
         devServer: {
-          static: {
-            directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
-          },
-          port: 3000, // Port thay cho port mặc định (8080)
-          open: true, // Mở trang webpack khi chạy terminal
-          hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-          compress: true, // Bật Gzip cho các tài nguyên
-          historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
-        }
-      }
-}
+            static: {
+                directory: 'dist', // Đường dẫn tương đối đến với thư mục chứa index.html
+            },
+            port: 3000, // Port thay cho port mặc định (8080)
+            open: true, // Mở trang webpack khi chạy terminal
+            hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+            compress: true, // Bật Gzip cho các tài nguyên
+            historyApiFallback: true, // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+        },
+    };
+};
