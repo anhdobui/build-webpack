aaac5608deb2d705068158cc75c56ce99191a970
anhdobui <anhdobui2002@gmail.com>
2023-01-29 13:08:00 +0700

    add Webpack Bundle Analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = (env) => {
     const isDevelopment = Boolean(env.development)
+    const basePlugins = [
+        new MiniCssExtractPlugin({
+          filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+          title: 'Webpack App',
+          filename: 'index.html',
+          template: 'src/template.html'
+        })
+      ]
+      const plugins = isDevelopment
+    ? basePlugins
+    : [...basePlugins, new BundleAnalyzerPlugin()]
     return {
         mode: isDevelopment ? 'development' : 'production',
         entry: {
@@ -54,16 +69,7 @@ module.exports = (env) => {
           ]
         },
         devtool: isDevelopment ? 'source-map' : false,
-        plugins: [
-          new MiniCssExtractPlugin({
-              filename: '[name].[contenthash].css'
-          }),
-          new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: 'src/template.html'
-          })
-        ],
+        plugins,
         devServer: {
           static: {
             directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
